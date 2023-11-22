1f8a97062779de60972380575b43ae5908f05d51
unknown <sriaravindansiva@gmail.com>
2023-11-22 16:30:14 +0530

    upto  strings

--- sample.js
+++ sample.js
@@ -1,1 +1,34 @@
-console.log("hello")
+/* //string,Number,boolean,null,undefiened
+
+const name = 'john';
+const age=30;
+const rating=4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+
+console.log(typeof x);
+
+//concatenation
+console.log('my name is'+ name + 'and i am' + age );
+
+//template string
+const hello=(`my name is ${name} and i am ${age}`);
+
+console.log(hello); */
+
+//string
+
+const s="hello world";
+
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.substring(0, 5));
+console.log(s.substring(0, 5).toUpperCase());
+console.log(s.split(''));
+console.log(s.split(', '));
+
+//Arrays
+
+
